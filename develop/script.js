71aada0e3db2ebb13d4f9d0b28055a116c6d4f2f
Jimmys Mac <eadesj25@gmail.com>
2023-03-20 20:44:39 -0400

    got saveBtn working

--- develop/script.js
+++ develop/script.js
@@ -2,8 +2,6 @@ $(function () {
   var today = dayjs();
   $('#currentDay').text(today.format('MMM DD, YYYY'));
 
- 
-
   $('.time-block').each(function () {
     var currentHour = dayjs().hour();
     var hour = parseInt($(this).attr('id').replace('hour-', ''));
@@ -19,10 +17,21 @@ $(function () {
 
   });
 
- 
+  $('.time-block').each(function () {
+    var hour = $(this).attr('id');
+    var description = localStorage.getItem(hour);
+
+    $('.saveBtn').on('click', function () {
+      var hour = $(this).parent().attr('id');
+      var description = $(this).siblings('.description').val();
+      localStorage.setItem(hour, description);
+    })
+
+
+    if (description) {
+      $(this).children('.description').val(description);
+    }
+  })
+  
 });
-
-
-
-
 
