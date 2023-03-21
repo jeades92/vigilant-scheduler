45b55c60e98f2ad67ffe1b2f268114e5caaf9053
Jimmys Mac <eadesj25@gmail.com>
2023-03-21 16:45:16 -0400

    add/remove comments & format document

--- develop/script.js
+++ develop/script.js
@@ -1,7 +1,9 @@
+//initializes function, creates variable today and assigns to current date of dayjs library
 $(function () {
   var today = dayjs();
   $('#currentDay').text(today.format('MMM DD, YYYY'));
 
+//assigns css to each classs of past, present, and future  
   $('.time-block').each(function () {
     var currentHour = dayjs().hour();
     var hour = parseInt($(this).attr('id').replace('hour-', ''));
@@ -17,17 +19,19 @@ $(function () {
 
   });
 
+//retrieves value with localStorage ID & assigns to description  
   $('.time-block').each(function () {
     var hour = $(this).attr('id');
     var description = localStorage.getItem(hour);
 
+//binds click event to all elements with saveBtn class, saves to local storage    
     $('.saveBtn').on('click', function () {
       var hour = $(this).parent().attr('id');
       var description = $(this).siblings('.description').val();
       localStorage.setItem(hour, description);
     })
 
-
+//sets value of description to current element value in localStorage
     if (description) {
       $(this).children('.description').val(description);
     }
